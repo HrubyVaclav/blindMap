f82d2159892c758ab8edf81d4c6e01dde85b59d3
hruby <hruby.vaclav21@gmail.com>
2026-03-08 10:15:13 +0100

    init commit

---
+++ src/main/java/cz/hruby/blindmapbackend/BlindMapBackendApplication.java
@@ -0,0 +1,13 @@
+package cz.hruby.blindmapbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlindMapBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlindMapBackendApplication.class, args);
+    }
+
+}
